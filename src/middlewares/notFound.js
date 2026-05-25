da6d8800a5936ff5832a371d97e4f28179041f9c
Maham Maryam <152018095+Maham38@users.noreply.github.com>
2026-05-25 11:51:40 -0700

    Delete src directory

--- src/middlewares/notFound.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const ApiError = require('../utils/ApiError');
-
-const notFound = (req, res, next) => {
-  next(new ApiError(404, `Route not found: ${req.method} ${req.originalUrl}`));
-};
-
-module.exports = notFound;
--- src/utils/sendResponse.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const sendResponse = (res, statusCode, data, message = 'Success') => {
-  res.status(statusCode).json({ success: true, message, data });
-};
-
-module.exports = sendResponse;
